a495ad58ff686666767e0daaa31d86acfaed369f
Adam Greig <random@randomskk.net>
2009-07-09 03:11:34 +0100

    started to get some kinda gui. sucks though.

--- trendstream.py
+++ trendstream.py
@@ -4,13 +4,13 @@ import base64
 import socket
 import urllib
 import urllib2
+import Tkinter
 import simplejson
 
 # get auth data
 username = raw_input("Twitter username: ")
 password = raw_input("Twitter password: ")
 auth_str = base64.b64encode(username + ":" + password)
-print auth_str
 
 # fetch twitter trends
 trends_url = "http://search.twitter.com/trends.json"
@@ -22,27 +22,39 @@ for trend in json['trends']:
 
 # encode the trends as an http post request
 post_data = urllib.urlencode([('track', ','.join(trends))])
-print trends
 
 # open the stream
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 sock.connect(('stream.twitter.com', 80))
 sock.send('POST /track.json HTTP/1.1\r\n')
-sock.send('Authorization: Basic %s=\r\n' % auth_str)
+sock.send('Authorization: Basic %s\r\n' % auth_str)
 sock.send('User-Agent: Python-socket\r\n')
 sock.send('Host: stream.twitter.com\r\n')
 sock.send('Accept: */*\r\n')
-sock.send('Content-Length: 16\r\n')
+sock.send('Content-Length: %s\r\n' % str(len(post_data)))
 sock.send('Content-Type: application/x-www-form-urlencoded\r\n')
 sock.send('\r\n')
 sock.send(post_data)
 
-while 1:
+
+root = Tkinter.Tk()
+w = Tkinter.Label(root, text="Getting tweets...")
+w.pack()
+
+def update_tweet():
+    global sock
+    global w
+    global root
     buf = sock.recv(4096)
-    print buf
     try:
         lines = buf.split('\r\n')
         j = simplejson.loads(lines[1])
         print j['user']['screen_name'] + ': ' + j['text']
+        w['text'] = j['user']['screen_name'] + ': ' + j['text']
     except ValueError, TypeError:
         pass
+    root.after(1, update_tweet)
+
+root.after_idle(update_tweet)
+
+root.mainloop()
